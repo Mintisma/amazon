2f1a8e7b8d8b74101f9305d28085674779ee4b32
Mintisama <1009890615@qq.com>
2020-01-08 18:00:07 +0800

    sku_ad

---
+++ sku_advertised_info/get_sku_info.py
@@ -0,0 +1,50 @@
+import time
+
+import requests
+from scrapy import Selector
+from selenium import webdriver
+
+from sqlHelper import Sqlhelper
+
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'}
+
+sqlHelper = Sqlhelper()
+base_url = 'https://www.amazon.com/dp/'
+TABLE = 'advertised_listing_info'
+
+
+def get_asin_data(asin):
+    url = base_url + asin
+    browser = webdriver.Chrome(executable_path='/Users/ted/Desktop/chromedriver')
+    browser.get(url)
+
+    time.sleep(10)
+    # r = requests.get(url, headers=headers)
+    selector = Selector(text=browser.page_source)
+    browser.close()
+
+    title = selector.xpath('//span[@id="productTitle"]/text()').extract_first('').strip()
+    bullet_points = selector.xpath('//span[@class="a-list-item"]/text()').extract()
+    bullet_point_list = [bullet_point.strip() for bullet_point in bullet_points if len(bullet_point.strip()) > 0]
+    bullet_points = ' '.join(bullet_point_list)
+
+    data = {
+        'title': title,
+        'bullet_points': bullet_points,
+        'asin': asin,
+    }
+    keys = ', '.join(data.keys())
+    values = ', '.join(['%s'] * len(data))
+    # 插入数据库
+    insert_sql = 'INSERT INTO {table} ({keys}) VALUES ({values}) ON DUPLICATE KEY UPDATE'.format(table=TABLE, keys=keys, values=values)
+    update = ','.join([" {key}=VALUES ({key})".format(key=key) for key in list(data.keys())])
+    insert_sql += update
+
+    sqlHelper.modify(insert_sql, args=tuple(data.values(), ))
+    sqlHelper.close()
+
+
+if __name__ == '__main__':
+    asin = input('please input the ASIN you want to advertise \n')
+    get_asin_data(asin)
+
