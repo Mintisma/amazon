b8ae7b185088802acf5639c1ecd5b06914751c18
Mintisma <1009890615@qq.com>
2020-03-13 14:25:40 +0800

    抓取被广告产品sku时，使用服务器的mysql

--- sku_advertised_info/settings.py
+++ sku_advertised_info/settings.py
@@ -1,6 +1,12 @@
+# db = {
+#     'HOST': 'localhost',
+#     'USER': 'root',
+#     'PASSWORD': 'zz6901877',
+#     'NAME': 'test',
+# }
 db = {
-    'HOST': 'localhost',
+    'HOST': '52.82.28.224',
     'USER': 'root',
-    'PASSWORD': 'zz6901877',
+    'PASSWORD': 'sellermotor',
     'NAME': 'test',
 }
